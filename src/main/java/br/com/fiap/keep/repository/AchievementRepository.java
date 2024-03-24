beff67cf2cb15c14faf23af99c927e48b1ee53f0
Gustavo <111543305+gustavo-sorrilha@users.noreply.github.com>
2024-03-24 11:07:12 -0300

    Delete src/main/java/br/com/fiap/keep/repository directory

--- src/main/java/br/com/fiap/keep/repository/AchievementRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.keep.repository;
-
-import br.com.fiap.keep.model.Achievement;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AchievementRepository extends JpaRepository<Achievement, Long> {
-}
