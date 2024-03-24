8f5727cbb055c1bbd332e2b99d0a274b2bfb657b
Gustavo <111543305+gustavo-sorrilha@users.noreply.github.com>
2024-03-24 11:09:40 -0300

    Update AchievementController.java

--- src/main/java/br/com/fiap/keep/Controller/AchievementController.java
+++ src/main/java/br/com/fiap/keep/Controller/AchievementController.java
@@ -1,30 +1,24 @@
 package br.com.fiap.keep.Controller;
 
-import br.com.fiap.keep.model.Achievement;
-import br.com.fiap.keep.repository.AchievementRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
-@RequestMapping("/achievement")
+@RequestMapping("achievements")
 public class AchievementController {
-
-    private final AchievementRepository achievementRepository;
-
-    @Autowired
-    public AchievementController(AchievementRepository achievementRepository) {
-        this.achievementRepository = achievementRepository;
-    }
 
     @GetMapping
     public String index(Model model){
-        List<Achievement> achievements = achievementRepository.findAll();
-        model.addAttribute("achievements", achievements);
-        return "achievement/index";
+        model.addAttribute("goals", List.of(
+                "Desenvolver Interface de Login",
+                "Implementar CRUD de Usuários",
+                "Configurar Autenticação JWT",
+                "Criar Testes de Integração"
+        ));
+        return "task/index";
     }
+
 }
