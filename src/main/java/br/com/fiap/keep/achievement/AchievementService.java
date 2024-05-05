4da2103a82176c2bd60a81968f08d0f40ddab0d2
Exxtacsy <RM97068@fiap.com.br>
2024-05-05 10:01:41 -0300

    service

---
+++ src/main/java/br/com/fiap/keep/achievement/AchievementService.java
@@ -0,0 +1,69 @@
+package br.com.fiap.keep.achievement;
+
+import br.com.fiap.epictask.task.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.keep.user.User;
+import br.com.fiap.keep.user.UserService;
+
+@Service
+public class AchievementService {
+
+    @Autowired
+    TaskRepository repository;
+
+    @Autowired
+    UserService userService;
+
+    public void catchTask(Long id, User myuser) {
+        var task = repository.findById(id).orElseThrow(
+            () -> new IllegalArgumentException("tarefa não encontrada")
+        );
+
+        if(task.getUser() != null)
+            throw new IllegalArgumentException("tarefa já atribuída");
+
+        task.setUser(myuser);
+        repository.save(task);
+    }
+
+    public void dropTask(Long id, User myuser) {
+        var task = repository.findById(id).orElseThrow(
+            () -> new IllegalArgumentException("tarefa não encontrada")
+        );
+
+        if(task.getUser() != myuser)
+            throw new IllegalArgumentException("tarefa atribuída a outro usuário");
+
+
+        task.setUser(null);
+        repository.save(task);
+    }
+
+    public void inc(Long id, User myuser) {
+        var task = repository.findById(id).orElseThrow(
+            () -> new IllegalArgumentException("tarefa não encontrada")
+        );
+        if (task.getStatus() +10 > 100) return;
+        task.setStatus(task.getStatus() + 10);
+        
+        if (task.getStatus() == 100){
+            userService.addScore(myuser, task.getScore());
+        }
+        
+        repository.save(task);
+    }
+
+    public void dec(Long id, User myuser) {
+        var task = repository.findById(id).orElseThrow(
+            () -> new IllegalArgumentException("tarefa não encontrada")
+        );
+
+        if (task.getStatus() - 10 < 0) return;
+
+        task.setStatus(task.getStatus() - 10);
+        repository.save(task);
+    }
+
+}
